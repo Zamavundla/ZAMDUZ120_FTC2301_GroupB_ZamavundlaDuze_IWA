bcff80bb5080635a250441bdc88be79f47781089
Zamavundla <zsdouzee8@gmail.com>
2023-05-05 15:59:57 +0200

    final book preview commit

--- Interactive-Web-Apps/IWA_19/book-connect-main/scripts.js
+++ Interactive-Web-Apps/IWA_19/book-connect-main/scripts.js
@@ -13,7 +13,7 @@
 // Import data from data.js and handlers.
 import { BOOKS_PER_PAGE, authors, genres, books } from "./data.js"
 import { html, } from "./handlers.js";
-import { createPreviewsFragment, updateRemaining, showBookDescription } from "./handlers.js"
+import { createPreviewsFragment, updateRemaining, showBookDescription, showPreview, createGenres} from "./handlers.js"
 
 const range = [0, 36]
 const matches = books; // Try to convert matches to an indepenedent copy.
